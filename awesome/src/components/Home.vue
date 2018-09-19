da9972259e5a00ded7291f170974a4fa9f952cd4
Christopher Fontaine <c.j.fontaine@gmail.com>
2018-09-18 23:42:16 -0400

    Completed UI

---
+++ awesome/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container fluid>
+        <v-layout row wrap>
+            <v-flex xs12 class="text-xs-center" mt-5>
+                <h1>Home Page</h1>
+            </v-flex>
+            <v-flex xs12 class="text-xs-center" mt-3>
+                <p>This is a User's Home Page</p>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
