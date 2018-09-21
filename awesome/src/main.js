be9cc6467f225b26da88e1d4dde9a441fe23c699
Christopher Fontaine <c.j.fontaine@gmail.com>
2018-09-21 00:37:35 -0400

    Completed tutorial

--- awesome/src/main.js
+++ awesome/src/main.js
@@ -6,15 +6,33 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import { store } from './store'
+import firebase from 'firebase'
 
 Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+  apiKey: 'AIzaSyD6UmvLZcElbe0of2L52LShoGpqpOWpY5s',
+  authDomain: 'awesome-3ffcc.firebaseapp.com',
+  databaseURL: 'https://awesome-3ffcc.firebaseio.com',
+  projectId: 'awesome-3ffcc',
+  storageBucket: 'awesome-3ffcc.appspot.com',
+  messagingSenderId: '583124458916'
+})
+
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
+const unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(App),
+    created () {
+      if (firebaseUser) {
+        store.dispatch('autoSignIn', firebaseUser)
+      }
+    }
+  })
+  unsubscribe()
 })
